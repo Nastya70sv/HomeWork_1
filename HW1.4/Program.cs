107ca444975bb2f1d7e253685ac4ee24ffae583d
Anastasia <nastya70sv@mail.ru>
2022-11-15 16:22:27 +0300

    Added HW 1.4

---
+++ HW1.4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count = 2;
+
+if(number > 1)
+{
+     while(count <= number)
+{
+     Console.Write(count + " ");
+     count = count + 2;
+}
+}
